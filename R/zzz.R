d7ba47c446cb446279a7521c35f862e8ab7a5cc5
Lilin Yin <1009926399@qq.com>
2019-11-21 17:02:08 +1000

    Add files via upload

---
+++ R/zzz.R
@@ -0,0 +1,4 @@
+.onAttach <- function(...){
+    packageStartupMessage("Full description, Bug report, Suggestion and the latest codes:")
+    packageStartupMessage("https://github.com/YinLiLin/hibayes")
+}
