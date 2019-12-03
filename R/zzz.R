16596b554bd6a364b816b641215c7fcd28470de0
YinLiLin <l.yin@imb19-014155-lt.imb.uq.edu.au>
2019-12-03 15:37:56 +1000

    delete

--- R/zzz.R
+++
@@ -1,4 +0,0 @@
-.onAttach <- function(...){
-    packageStartupMessage("Full description, Bug report, Suggestion and the latest codes:")
-    packageStartupMessage("https://github.com/YinLiLin/hibayes")
-}
